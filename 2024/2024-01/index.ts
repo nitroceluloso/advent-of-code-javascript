e30688896f17bec8cbf42b630238cd4f33c2ebe1
Cristóbal Sepúlveda <cristobalsepulveda1@hotmail.com>
2025-03-18 18:51:58 -0300

    add 2024 day 01.

---
+++ 2024/2024-01/index.ts
@@ -0,0 +1,60 @@
+/*
+Implementation for the first day of AOC.
+Link: https://adventofcode.com/2024/day/1
+*/
+
+function parseContent(file: string) {
+  const listRaw = file.trim().split("\n");
+  const listLeft: number[] = [];
+  const listRight: number[] = [];
+
+  listRaw.forEach((row) => {
+    const [left, right] = row.split("   ");
+
+    listLeft.push(Number(left));
+    listRight.push(Number(right));
+  });
+
+  return [listLeft, listRight];
+}
+
+function calculateSimilarities(leftNumbers: number[], rightNumbers: number[]) {
+  const foundDictionary = new Map<number, number>();
+  const similarities: number[] = [];
+
+  leftNumbers.forEach((leftElement) => {
+    let similarity = null;
+    if (!foundDictionary.has(leftElement)) {
+      const count = rightNumbers.filter(
+        (rightElement) => rightElement === leftElement,
+      ).length;
+      foundDictionary.set(leftElement, count);
+      similarity = count;
+    }
+    similarity = similarity ?? (foundDictionary.get(leftElement) as number);
+    similarities.push(leftElement * similarity);
+  });
+
+  return similarities.reduce((prev, act) => prev + act, 0);
+}
+
+(async function () {
+  const distance: number[] = [];
+  const file = Bun.file("input.txt");
+  const fileText = await file.text();
+  const [left, right] = parseContent(fileText);
+
+  left.sort();
+  right.sort();
+
+  left.forEach((leftEl, idx) => {
+    distance.push(Math.abs(leftEl - right[idx]));
+  });
+
+  const firstQuestion = distance.reduce((prev, act) => prev + act, 0);
+  const secondQuestion = calculateSimilarities(left, right);
+
+  console.log("What is the total distance between your lists?:", firstQuestion);
+
+  console.log("What is their similarity score?:", secondQuestion);
+})();
